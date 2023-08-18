6b1a685cab8a4a7b167e4d4c2634ba8cc577a06f
chestercheng <im.chestercheng@icloud.com>
2023-08-18 21:24:25 +0800

    style: adjust banner image max height

--- content/2023/components/Banner.tsx
+++ content/2023/components/Banner.tsx
@@ -16,7 +16,7 @@ const Banner = ({ title, subTitle, children }: BannerProps) => {
       id="banner"
       className="tw-relative tw-flex tw-flex-col tw-items-center tw-justify-center tablet:tw-flex-row tw-mt-14"
     >
-      <Image className="tablet:tw-max-h-200 tw-w-full" src={bkgImg} alt="banner" />
+      <Image className="tablet:tw-max-h-screen tw-w-full" src={bkgImg} alt="banner" />
       <div className="tw-absolute tw-prose-lg tw-text-left tw-font-yk tw-text-black tw-leading-none tablet:tw-mr-80">
         <p className="tw-text-4xl tablet:tw-text-6xl tw-mb-1">{title}</p>
         <p className="tw-text-2xl tablet:tw-text-3xl tw-mt-1">{subTitle}</p>
