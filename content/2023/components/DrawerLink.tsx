0cf6c1ce12d6ec5cfe57af7948d6b6d65d78900a
5x <longhorn482653@gmail.com>
2023-08-20 18:36:50 +0800

    refactor(components/DrawerLink.tsx): remove unused code

--- content/2023/components/DrawerLink.tsx
+++ content/2023/components/DrawerLink.tsx
@@ -1,8 +1,4 @@
-'use client';
-
-import clsx from 'clsx';
 import NextLink from 'next/link';
-import { usePathname } from 'next/navigation';
 
 const DrawerLink = ({
   children,
@@ -11,18 +7,9 @@ const DrawerLink = ({
   children: React.ReactNode;
   to: string;
 }) => {
-  const pathname = usePathname();
-  const isActive = pathname === to;
   return (
     <NextLink className="tw-block tw-w-full tw-font-yk" href={to}>
-      <div
-        className={clsx(
-          'tw-px-8 tw-py-2 tw-text-2xl tw-font-medium tw-leading-loose tw-text-neutral-900 hover:tw-text-sky-600',
-          {
-            'tw-text-black': isActive,
-          },
-        )}
-      >
+      <div className="tw-px-8 tw-py-2 tw-text-2xl tw-font-medium tw-leading-loose tw-text-black hover:tw-text-sky-600">
         {children}
       </div>
     </NextLink>
