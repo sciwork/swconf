a6e77e296f91c892377ee453734c3dbabf797b4d
Zhang Wuxian <wuxian@tenmax.io>
2023-10-21 21:50:13 +0800

    feat: get schedule api

--- content/2023/app/program/[day]/page.tsx
+++ content/2023/app/program/[day]/page.tsx
@@ -2,22 +2,22 @@ import { Metadata } from 'next';
 import Article from '@/components/Article';
 import ScheduleComponent from '@/components/Schedule';
 import ToBeAnnounced from '@/components/ToBeAnnounced';
-import { DayType } from '@/models/Schedule';
+import { getSchedule } from '@/utils/apis';
 import dayjs from '@/utils/day';
 
 export const metadata: Metadata = {
   title: 'Programs',
 };
 
-export function generateStaticParams() {
-  // TODO: Fetch schedule from pretalx API
-  const days: DayType[] = [];
+export async function generateStaticParams() {
+  const schedule = await getSchedule();
+  const days = schedule?.conference?.days ?? [];
   return days.map((day) => ({ day: day.date.format('YYYY-MM-DD') }));
 }
 
-const Page = ({ params }: { params: { day: string } }) => {
-  // TODO: Fetch schedule from pretalx API
-  const days: DayType[] = [];
+const Page = async ({ params }: { params: { day: string } }) => {
+  const schedule = await getSchedule();
+  const days = schedule?.conference?.days ?? [];
   const currentDay = dayjs(params.day);
 
   return (
--- content/2023/app/program/page.tsx
+++ content/2023/app/program/page.tsx
@@ -2,27 +2,15 @@ import { Metadata } from 'next';
 import Article from '@/components/Article';
 import ScheduleComponent from '@/components/Schedule';
 import ToBeAnnounced from '@/components/ToBeAnnounced';
-import RawSchedule from '@/models/RawSchedule';
-import Schedule, { DayType } from '@/models/Schedule';
+import { getSchedule } from '@/utils/apis';
 
 export const metadata: Metadata = {
   title: 'Programs',
 };
 
-const getDays = (json: any): DayType[] => {
-  if (!json) {
-    return [];
-  }
-
-  const rawSchedule = RawSchedule.fromJson(json);
-  const schedule = Schedule.fromRaw(rawSchedule);
-  return schedule.conference.days;
-};
-
-const Page = () => {
-  // TODO: Fetch schedule from pretalx API
-  const scheduleJson = null;
-  const days = getDays(scheduleJson);
+const Page = async () => {
+  const schedule = await getSchedule();
+  const days = schedule?.conference?.days ?? [];
 
   return (
     <Article>
