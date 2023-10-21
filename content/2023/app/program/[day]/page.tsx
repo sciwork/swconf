38b0cb315738765962045e300a60f666c457d4c4
Zhang Wuxian <wuxian@tenmax.io>
2023-10-21 22:17:20 +0800

    feat: client side data fetch

--- content/2023/app/program/[day]/page.tsx
+++ content/2023/app/program/[day]/page.tsx
@@ -1,13 +1,12 @@
-import { Metadata } from 'next';
+'use client';
+
+import { useEffect, useState } from 'react';
 import Article from '@/components/Article';
 import ScheduleComponent from '@/components/Schedule';
 import ToBeAnnounced from '@/components/ToBeAnnounced';
+import { DayType } from '@/models/Schedule';
 import { getSchedule } from '@/utils/apis';
 import dayjs from '@/utils/day';
-
-export const metadata: Metadata = {
-  title: 'Programs',
-};
 
 export async function generateStaticParams() {
   const schedule = await getSchedule();
@@ -15,10 +14,24 @@ export async function generateStaticParams() {
   return days.map((day) => ({ day: day.date.format('YYYY-MM-DD') }));
 }
 
-const Page = async ({ params }: { params: { day: string } }) => {
-  const schedule = await getSchedule();
-  const days = schedule?.conference?.days ?? [];
+const Page = ({ params }: { params: { day: string } }) => {
   const currentDay = dayjs(params.day);
+  const [days, setDays] = useState<DayType[]>([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchSchedule() {
+      const schedule = await getSchedule();
+      setDays(schedule?.conference?.days ?? []);
+      setLoading(false);
+    }
+
+    fetchSchedule();
+  }, []);
+
+  if (isLoading) {
+    return <Article>Loading...</Article>;
+  }
 
   return (
     <Article>
--- content/2023/app/program/page.tsx
+++ content/2023/app/program/page.tsx
@@ -1,16 +1,29 @@
-import { Metadata } from 'next';
+'use client';
+
+import { useEffect, useState } from 'react';
 import Article from '@/components/Article';
 import ScheduleComponent from '@/components/Schedule';
 import ToBeAnnounced from '@/components/ToBeAnnounced';
+import { DayType } from '@/models/Schedule';
 import { getSchedule } from '@/utils/apis';
 
-export const metadata: Metadata = {
-  title: 'Programs',
-};
+const Page = () => {
+  const [days, setDays] = useState<DayType[]>([]);
+  const [isLoading, setLoading] = useState(true);
 
-const Page = async () => {
-  const schedule = await getSchedule();
-  const days = schedule?.conference?.days ?? [];
+  useEffect(() => {
+    async function fetchSchedule() {
+      const schedule = await getSchedule();
+      setDays(schedule?.conference?.days ?? []);
+      setLoading(false);
+    }
+
+    fetchSchedule();
+  }, []);
+
+  if (isLoading) {
+    return <Article>Loading...</Article>;
+  }
 
   return (
     <Article>
