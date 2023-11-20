d73a92c30c94298fcd2530c3ff225777b070d278
5x <longhorn482653@gmail.com>
2023-11-20 22:34:51 +0800

    feat: new teams layout

--- content/2023/app/about/page.tsx
+++ content/2023/app/about/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
+import teams from '@/configurations/teams';
 import Article from '@/components/Article';
-import Content from '@/components/Content';
+import Avatar from '@/components/Avatar';
 
 export const metadata: Metadata = {
   title: 'About',
@@ -9,7 +10,21 @@ export const metadata: Metadata = {
 const Page = () => {
   return (
     <Article>
-      <Content name="about" />
+      {teams.map((subTeam) => (
+        <>
+          {subTeam.title && <h1>{subTeam.title}</h1>}
+          <section className="tw-grid tw-grid-flow-row tw-grid-cols-1 md:tw-grid-cols-2 xl:tw-grid-cols-3 2xl:tw-grid-cols-4 tw-gap-4 tw-justify-items-center tw-mb-16 xl:tw-mb-32">
+            {subTeam.members.map((member) => (
+              <Avatar
+                key={member.name}
+                image={member.image}
+                name={member.name}
+                email={member.email}
+              />
+            ))}
+          </section>
+        </>
+      ))}
     </Article>
   );
 };
---
+++ content/2023/configurations/teams.ts
@@ -0,0 +1,118 @@
+import ChunYuTsengImg from '@/assets/sw23-staff-avatar/chun-yu-tseng.jpg';
+import enImg from '@/assets/sw23-staff-avatar/en.jpeg';
+import enzoImg from '@/assets/sw23-staff-avatar/enzo.jpeg';
+import liAnChenImg from '@/assets/sw23-staff-avatar/li-an-chen.jpg';
+import liangBoWangImg from '@/assets/sw23-staff-avatar/liang-bo-wang.jpg';
+import liliImg from '@/assets/sw23-staff-avatar/lili.jpg';
+import manusnKuoImg from '@/assets/sw23-staff-avatar/manusn-kuo.jpg';
+import marsImg from '@/assets/sw23-staff-avatar/mars.jpg';
+import peterWolfImg from '@/assets/sw23-staff-avatar/peter-wolf.png';
+import timHsuImg from '@/assets/sw23-staff-avatar/tim-hsu.jpeg';
+import yycImg from '@/assets/sw23-staff-avatar/yyc.jpeg';
+
+const teams = [
+  {
+    title: 'Core Team',
+    members: [
+      {
+        name: 'Yung-Yu Chen',
+        email: 'yyc@sciwork.dev',
+        image: yycImg,
+      },
+      {
+        name: 'Shu-Hsi Lin',
+        email: 'suci@sciwork.dev',
+      },
+      {
+        name: 'Robert Lin',
+        email: 'rlin@sciwork.dev',
+      },
+      {
+        name: 'Chester Cheng',
+        email: 'chester@sciwork.dev',
+      },
+      {
+        name: 'Jenny Yen',
+        email: 'jennyyen@sciwork.dev',
+      },
+    ],
+  },
+  {
+    title: 'Program',
+    members: [
+      { name: 'Shuhsi', email: 'suci@sciwork.dev' },
+      { name: 'Yu-Hsun Lee' },
+      { name: 'Zong-han Xi' },
+      { name: 'Huei-Yuan Su', image: marsImg },
+      {
+        name: 'Li-An Chen',
+        image: liAnChenImg,
+      },
+    ],
+  },
+  {
+    title: 'Reviewer',
+    members: [
+      { name: 'Yung-Yu Chen', email: 'yyc@sciwork.dev', image: yycImg },
+      { name: 'Shuhsi', email: 'suci@sciwork.dev' },
+      { name: 'Yu-Hsun Lee' },
+      { name: 'Zong-Han Xi' },
+      { name: 'Huei-Yuan Su (Mars)', image: marsImg },
+      {
+        name: 'Li-An Chen',
+        image: liAnChenImg,
+      },
+      { name: 'Liang-Bo Wang', image: liangBoWangImg },
+      { name: 'Kunxian Huang' },
+      {
+        name: 'Wen-Chung (Tim) Hsu',
+        image: timHsuImg,
+      },
+      { name: 'Chun-Yu Tseng (Joe)', image: ChunYuTsengImg },
+      {
+        name: 'Wang Wen-jet (Peter. w)',
+        image: peterWolfImg,
+      },
+      { name: 'Alyssa Chen' },
+      {
+        name: 'Chung-Pu (Enzo) Chang',
+        image: enzoImg,
+      },
+      {
+        name: 'Mansun Kuo',
+        image: manusnKuoImg,
+      },
+      { name: 'CL (Chia-Liang) Kao' },
+      { name: 'Edward Hong' },
+      { name: 'Carter Lin' },
+      { name: 'Rendy Kuo' },
+      { name: 'Li-Ho Hsu (Daren)' },
+    ],
+  },
+  {
+    title: 'Web & Design',
+    members: [
+      { name: 'Chaster', email: 'chester@sciwork.dev' },
+      { name: 'Wuxian' },
+      { name: 'Lili', image: liliImg },
+      { name: 'Steve' },
+      { name: 'tN' },
+      { name: 'EN', image: enImg },
+    ],
+  },
+  {
+    title: 'Finance & Venue',
+    members: [
+      { name: 'Robert', email: 'rlin@sciwork.dev' },
+      { name: 'Jenny', email: 'jennyyen@sciwork.dev' },
+      { name: 'Shanyen' },
+      { name: 'Vera' },
+    ],
+  },
+  {
+    title: 'Registration',
+    members: [{ name: 'Ming-Jie' }],
+  },
+];
+
+export default teams;
